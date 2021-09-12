2185d765c3de12cb64297e1b2f3e53edcd2ca083
Kameron Ashley <kameronashley@gmail.com>
2021-09-11 17:15:10 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(){
+    return (x += y)
+}
+function subtract() {
+    return (x -= y)
+}
+function divide() {
+    return (x /= y)
+}
+function add(a, b) {
+    return (a += b)
+}
+function subtract(a, b) {
+    return (a -= b)
+}
+function multiply(a, b) {
+    return (a *= b)
+}
+function divide(a, b) {
+    return (a /= b)
+}
+function increment(n) {
+    return (++n)
+}
+function decrement(n) {
+    return (--n)
+}
+function makeInt(n) {
+    return parseInt(n,10)
+}
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
